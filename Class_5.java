f0107639bb42af1a744de5a5b0f6192516bd528e
Dhruv <dhruvthakral26@gmail.com>
2024-03-18 08:13:57 +0530

    Class_5

---
+++ Class_5.java
@@ -0,0 +1,18 @@
+//         Operators And Operands- Class 5
+
+class WEEK1{
+    public static void main(String args[]){
+
+        int x = 5;
+        int y = 7;
+        int z = x+y;
+
+        int m = 5;
+        int n = m++;
+        System.out.println("M is: "+m + " , N is : "+n);
+
+        int p = 5;
+        int q = ++p;
+        System.out.println("P is: "+ p + " , Q is: "+q);
+    }
+}
